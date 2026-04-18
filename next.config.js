73550d7d2e2e22bd4b992f516b9ab6c37442db9f
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-18 12:52:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/odds-scanner', // DODAJ IME SVOG REPOZITORIJUMA OVDE
-    assetPrefix: '/odds-scanner', // DODAJ I OVDE
-    images: {
-        unoptimized: true,
-    },
+  output: 'export',
+  // Ovo govori Next-u da se sajt nalazi na /odds-scanner/ putanji
+  basePath: '/odds-scanner',
+  assetPrefix: '/odds-scanner',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
