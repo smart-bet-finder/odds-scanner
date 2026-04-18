bda0b99c34de3b9f58d5129f6bfd7b08608ee637
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-18 12:32:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['api.the-odds-api.com'],
-  },
-}
+    output: 'export',  // OVA LINIJA JE KLJUČNA
+    images: {
+        unoptimized: true, // Obavezno za statički export
+    },
+};
 
-module.exports = nextConfig
-
+export default nextConfig;
