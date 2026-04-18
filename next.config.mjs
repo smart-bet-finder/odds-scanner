b24d346b0abc48b2cef600b5b0df879c53ae42de
majavujovicns021 <majavujovicns021@gmail.com>
2026-04-18 12:50:11 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Ključno za GitHub Pages
-    images: {
-        unoptimized: true, // Obavezno jer GitHub Pages ne podržava Next.js optimizaciju slika
-    },
-};
-
-export default nextConfig;
